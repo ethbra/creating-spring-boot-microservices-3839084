b5d14927857c06b781e0cc00e73ef57d2b59272b
ethbra <143832482+ethbra@users.noreply.github.com>
2024-08-17 19:59:48 +0000

    finished declare JPA repo

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,12 @@
+
+package com.example.explorecalijpa.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.explorecalijpa.model.TourPackage;
+
+/**
+ * TourPackageRepository
+ */
+public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
+
+}

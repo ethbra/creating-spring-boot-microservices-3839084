b1de20ffe45abc528da695953a68bbddf5774b58
ethbra <143832482+ethbra@users.noreply.github.com>
2024-08-22 04:16:20 +0000

    Added explorecali_images with initializr

---
+++ explorecali-images/src/main/java/com/example/explorecali_images/ExplorecaliImagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.explorecali_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliImagesApplication.class, args);
+	}
+
+}
---
+++ explorecali-images/src/test/java/com/example/explorecali_images/ExplorecaliImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explorecali_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
